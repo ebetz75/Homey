54548c55fbb70952282287f03037f83cc36266de
ebetz75 <ebetz75@yahoo.com>
2025-11-20 17:38:46 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Polyfill process.env.API_KEY so the existing code works without changes.
-    // In Vercel settings, you will name your variable "VITE_API_KEY".
-    'process.env.API_KEY': 'import.meta.env.VITE_API_KEY'
-  }
 });
